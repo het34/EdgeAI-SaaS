91f0f4b44c484b26107174ed60528b0dfb570af1
het34 <hettamboli341@gmail.com>
2025-05-11 18:36:56 +0530

    1) Basic react Tailwind Setup using 2) Vite Created Navbar

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Layout from "./components/layout"
+
+function App() {
+
+  return (
+    <Layout title="EdgeAI">
+    </Layout>
+  )
+}
+
+export default App
---
+++ src/components/layout.tsx
@@ -0,0 +1,27 @@
+interface LayoutProps {
+    title: string;
+    children: React.ReactNode;
+
+}
+
+import React, { useEffect } from 'react'
+import { Navbar } from './elements/Navbar';
+import { Footer } from './elements/Footer';
+
+const Layout = ({ title, children }: LayoutProps) => {
+
+    useEffect(() => {
+        document.title = title
+    }, [title])
+    return (
+        <>
+            <Navbar />
+            <main className="flex flex-col fap-y-20 md:gap-y-32 overflow-hidden">
+                {children}
+            </main>
+            <Footer />
+        </>
+    )
+}
+
+export default Layout
